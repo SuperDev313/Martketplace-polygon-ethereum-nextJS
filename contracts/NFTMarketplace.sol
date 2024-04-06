d1a03c12672811cd62ac1e4f019be961e24f7da7
SuperDev313 <pablo313boss@gmail.com>
2024-04-06 08:01:53 -0700

    nft marketplace

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
+
+import "hardhat/console.sol"
