153d4196d8c20ecdcdb36c6a20e9c185b6115927
SuperDev313 <pablo313boss@gmail.com>
2024-04-06 12:42:24 -0700

    marketItem NFTMarketPlace

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,3 +7,23 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
 
 import "hardhat/console.sol"
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counter for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint listingPrice = 0.025 ether;
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+}
+
