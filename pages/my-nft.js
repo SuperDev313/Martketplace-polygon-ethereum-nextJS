fb12c408665f58e9daf47f2aaec39c519d5111d0
SuperDev313 <pablo313boss@gmail.com>
2024-04-09 18:26:51 -0700

    marketPlaceContract and data and item

--- pages/my-nft.js
+++ pages/my-nft.js
@@ -1,28 +1,56 @@
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import Web3Modal from 'web3modal'
-import { useRouter } from 'next/router'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import { useRouter } from "next/router";
 
-import {
-    marketplaceAddress
-} from '../config'
+import { marketplaceAddress } from "../config";
 
-
-import NFTMarketPlace from '../artifacts/contracts/NFTMarketPlace.sol/NFTMarketPlace.json'
+import NFTMarketPlace from "../artifacts/contracts/NFTMarketPlace.sol/NFTMarketPlace.json";
 
 export default function MyAssets() {
-    const [nfts, setNfts] = useState([])
-    const [loadingState, setLoadingState] = useState('not-loaded')
-    const router = useRouter()
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+  const router = useRouter();
 
-    useEffect(() => {
-        loadNFTs()
-    }, [])
-    
-    return (
-        <div className='flex justify-center'>
-            <div className='p-4'></div>
-        </div>
-    )
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    const web3Modal = new web3Modal.connect();
+    const provider = new ethers.provide.web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const marketplaceContract = new ethers.Contract(
+      marketplaceAddress,
+      NFTMarketPlace.abi,
+      signer
+    );
+
+    const data = await Promise.all(
+      data.map(async (i) => {
+        const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
+        const meta = await exios.formatUnits(i.price.toString(), "ether");
+
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          tokenURI,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+  return (
+    <div className="flex justify-center">
+      <div className="p-4"></div>
+    </div>
+  );
 }
