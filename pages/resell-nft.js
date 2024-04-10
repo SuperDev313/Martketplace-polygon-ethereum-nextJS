b6fda0d286e1b63319e40d246d9695279efb5991
SuperDev313 <pablo313boss@gmail.com>
2024-04-10 05:47:17 -0700

    resell nft

---
+++ pages/resell-nft.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+export default function ResellNFT() {
+  return <div className="flex justify-center"></div>;
+}
