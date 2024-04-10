042793f4d724589fd821353d96d7cfd3fd09ca70
SuperDev313 <pablo313boss@gmail.com>
2024-04-10 06:36:51 -0700

    list nft button

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { useRouter } from "next/router";
 import axios from "axios";
-import web3Modal from 'web3modal'
+import web3Modal from "web3modal";
 
-import { marketplaceAddress } from '.../config'
+import { marketplaceAddress } from ".../config";
 
 export default function ResellNFT() {
-  const [formInput, updateFormInput] = useState({price: '', image: ''});
+  const [formInput, updateFormInput] = useState({ price: "", image: "" });
   const router = useRouter();
   const { id, tokenURI } = router.query;
   const { image, price } = formInput;
@@ -20,12 +20,14 @@ export default function ResellNFT() {
         onChange={(e) =>
           updateFormInput({ ...formInput, price: e.target.value })
         }
-        {
-            image && (
-                <img className="rounded mt-4" width="350" src={image}/>
-            )
-        }
       />
+      {image && <img className="rounded mt-4" width="350" src={image} />}
+      <button
+        onClick={listNFTForSale}
+        className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+      >
+        List NFT
+      </button>
     </div>
   );
 }
