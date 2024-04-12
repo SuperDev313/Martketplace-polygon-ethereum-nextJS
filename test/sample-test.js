8aae9dab2d748b3126aca010e859a54b37ec4c35
SuperDev313 <pablo313boss@gmail.com>
2024-04-11 18:32:17 -0700

    test script

--- test/sample-test.js
+++ test/sample-test.js
@@ -4,10 +4,38 @@ describe("NFTMarket", function () {
     const nftMarketplace = await NFTMarketplace.deploy();
     await nftMarketplace.deployed();
 
-    let listingPrice = await nftMarketplace.getlistingPrice()
-    listingPrice = listingPrice.toString()
+    let listingPrice = await nftMarketplace.getlistingPrice();
+    listingPrice = listingPrice.toString();
 
-    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
-    
+    const auctionPrice = ethers.utils.parseUnits("1", "ether");
+
+    await nftMarketplace.createToken(
+      "https://www.mytokenlocation.com",
+      auctionPrice,
+      { value: listingPrice }
+    );
+    await nftMarketplace.createToken(
+      "htthps://www.mytokenlocation2.com",
+      auctionPrice,
+      { value: listingPrice }
+    );
+
+    // query for and return the unsold itesm
+
+    items = await nftMarketplace.fetehMarketItems();
+    items = await Promise.all(
+      items.map(async (i) => {
+        const tokenUri = await nftMarketplace.tokenURI(i.tokenId);
+        let item = {
+          price: i.price.toString(),
+          tokenId: i.tokenId.toString(),
+          seller: i.seller,
+          owner: i.owner,
+          tokenUri,
+        };
+        return item;
+      })
+    );
+    console.log("items:", items);
   });
 });
